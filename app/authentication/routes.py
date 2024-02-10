b7efe9416dd1cf966831c4256c09da6583ac903d
Aparna <aparna.ramphe@gmail.com>
2024-02-09 19:37:14 -0600

    Second commit

--- app/authentication/routes.py
+++ app/authentication/routes.py
@@ -1,4 +1,4 @@
-from forms import UserLogicForm
+from forms import UserLoginForm
 from models import User, db, check_password_hash
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 
